2cc7f300169523ee76a71b1617eb1ac80a045de7
kroy0910 <kameda.roy@gmail.com>
2019-08-07 18:34:48 +0900

    課題の提出p/l13

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(["prefix" => "admin"], function() {
-    Route::get("news/create","Admin\NewsController@add")->middleware('auth');
+Route::group(["prefix" => "admin", "midleware"=>"auth"], function() {
+    Route::get("news/create","Admin\NewsController@add");
+    Route::post("news/create","Admin\NewsController@create");
 });
 
 Route::group(["prefix"=>"admin/profile"],function(){
     Route::get("create","Admin\ProfileController@add")->middleware('auth');
+    Route::post("create","Admin\ProfileController@create");
     Route::get("edit","Admin\ProfileController@edit")->middleware('auth');
+    Route::post("edit","Admin\ProfileController@update");
 });
 
 
